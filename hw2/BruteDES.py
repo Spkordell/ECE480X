749efa498c67f228fb6eee6e9c6e8f6856645420
spkordell <spkordell@gmail.com>
2015-03-29 21:19:55 -0400

    Brute force DES cracker runs

--- hw2/BruteDES.py
+++ hw2/BruteDES.py
@@ -4,14 +4,19 @@
 
 from Crypto.Cipher import DES
 
-if __name__ == "__main__":
-    k = '0000000012345678'
-    key = k.decode('hex')
-
-    message = "Hello!!!"  
-    m = message.encode('hex')
-
-    a = DES.new(key)
-    ct = a.encrypt(m)
+if __name__ == "__main__":    
+    plaintext = '48656c6c6f212121'
+    ciphertext = '1f6339383e8da6c4'
+            
+    k = '0000000000000000'
     
-    print ct
+    ct = ''
+    while ct != ciphertext:
+        key = k.decode('hex')
+        a = DES.new(key)
+        ct = a.encrypt(plaintext.encode('hex'))   
+        print '{:016X}'.format(int(k, 16)) + ' : ' + ct
+       
+        #increment to next hex value'
+        k = '{:016X}'.format(int(k, 16)+1)
+    print "key is: " + key-1
