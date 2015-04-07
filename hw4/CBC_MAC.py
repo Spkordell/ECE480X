241b783442b357dd7d7d1c0bf44b107622c4886d
spkordell <spkordell@gmail.com>
2015-04-07 01:03:32 -0400

    Computing CBC_MAC using AES

--- hw4/CBC_MAC.py
+++ hw4/CBC_MAC.py
@@ -13,19 +13,22 @@ BLOCK_SIZE = 32 #number of bytes in an AES Block
 
 #append a pad of 1 followed by zeros until the message is evenly divisable by the block size
 def padMessage(message):
-    #print (len(message)/2)
     if ((len(message)/2) % BLOCK_SIZE):
         message += '80'
         while ((len(message)/2) % BLOCK_SIZE):
             message += '00'
-    #print (len(message)/2)   
     return message
 
 if __name__ == "__main__":    
-    #plaintext = raw_input('plaintext: ')
+    #message = raw_input('message: ')
+    #key = raw_input('key: ')
+    key = '00000000000000000000000000000000'
     message = "The quick brown fox jumps over the lazy dog"
-    #message = 'A'
-    #message = '00000000000000000000000000000000'
     
-    print padMessage(message.encode('hex'))
+    paddedMessage = padMessage(message.encode('hex'))
+    
+    cbc = AES.new(key.decode('hex'), AES.MODE_CBC, '00000000000000000000000000000000'.decode('hex'))
+    CBC_MAC = cbc.encrypt(paddedMessage).encode('hex')[-128:]
+    
+    print CBC_MAC
     
