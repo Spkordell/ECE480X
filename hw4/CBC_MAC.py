74e20f559c995d90e1f17f8d4f4231975e17dfa2
spkordell <spkordell@gmail.com>
2015-04-07 00:27:34 -0400

    Padding messages to be evenly divisable by block size.

--- hw4/CBC_MAC.py
+++ hw4/CBC_MAC.py
@@ -9,14 +9,23 @@
 
 from Crypto.Cipher import AES
 
+BLOCK_SIZE = 32 #number of bytes in an AES Block
 
+#append a pad of 1 followed by zeros until the message is evenly divisable by the block size
 def padMessage(message):
+    #print (len(message)/2)
+    if ((len(message)/2) % BLOCK_SIZE):
+        message += '80'
+        while ((len(message)/2) % BLOCK_SIZE):
+            message += '00'
+    #print (len(message)/2)   
     return message
 
 if __name__ == "__main__":    
     #plaintext = raw_input('plaintext: ')
     message = "The quick brown fox jumps over the lazy dog"
-
-    print padMessage(message)
-
+    #message = 'A'
+    #message = '00000000000000000000000000000000'
+    
+    print padMessage(message.encode('hex'))
     
