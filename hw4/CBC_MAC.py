c59c6e6cba273b2925cf6752c2a5742f20ca112a
spkordell <spkordell@gmail.com>
2015-04-06 23:40:26 -0400

    Started CBC_MAC

---
+++ hw4/CBC_MAC.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Project 4:
+# Name:Steven Kordell
+# Date: 04/06/2015
+# Mailbox number: 663
+
+
+from Crypto.Cipher import AES
+
+
+def padMessage(message):
+    return message
+
+if __name__ == "__main__":    
+    #plaintext = raw_input('plaintext: ')
+    message = "The quick brown fox jumps over the lazy dog"
+
+    print padMessage(message)
+
+    
