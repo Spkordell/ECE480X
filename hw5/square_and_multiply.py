3e4558b535a93605f4acbe773bc718ee3bdc3069
spkordell <spkordell@gmail.com>
2015-04-19 19:03:42 -0400

    Finished square and multiply algorithm

--- hw5/square_and_multiply.py
+++ hw5/square_and_multiply.py
@@ -9,24 +9,30 @@
 def my_pow(b,e,m):
     """ Computes b^e mod m using the square and multiply algorithm"""
     
-    x = b
-    n = m
-    
-    
+    #special case for things raised to the zeroth or first power
+    if e == 0:
+        return 1 % m
+    if e == 1:
+        return b % m
+        
+    #the square and multiply alorithm
     t = e.bit_length()        
-    r = x
+    r = b
     for i in range(t-2,-1,-1):
-        r = r**2 % n
+        r = r**2 % m
         if e & (1 << i):
-            r = r*x % n
+            r = r*b % m
     return r
 
-if __name__ == "__main__": 
-    #test a whole bunch of values   
+if __name__ == "__main__":    
+    #test a whole bunch of values      
     for i in range (0,1024):
         for j in range (0,1024):
             for k in range (1,1024):
                 a = pow(i,j,k)
                 b = my_pow(i,j,k)
                 if a != b:
-                    print str(a)+" "+str(b)+" "+str(i)+" "+str(j)+" "+str(k)                   
+                    print str(a)+" "+str(b)+" "+str(i)+" "+str(j)+" "+str(k)                 
+                    
+                    
+    
